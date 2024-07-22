370fb997edba3ec9f7756c154567f1f222a5acd4
hahabuhuiwan <155300244+hahabuhuiwan@users.noreply.github.com>
2024-07-22 18:55:10 +0800

    Update page.tsx

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -1,68 +1,14 @@
-export const metadata = {
-  title: "Sign In - Simple",
-  description: "Page description",
-};
+<!DOCTYPE html>
+<html>
 
-import Link from "next/link";
+<head>
+  <meta charset="UTF-8">
+  <title>我的简单网页3</title>
+</head>
 
-export default function SignIn() {
-  return (
-    <>
-      <>
-        <div className="mb-10">
-          <h1 className="text-4xl font-bold">Sign in to your account</h1>
-        </div>
-        {/* Form */}
-        <form>
-          <div className="space-y-4">
-            <div>
-              <label
-                className="mb-1 block text-sm font-medium text-gray-700"
-                htmlFor="email"
-              >
-                Email
-              </label>
-              <input
-                id="email"
-                className="form-input w-full py-2"
-                type="email"
-                placeholder="corybarker@email.com"
-                required
-              />
-            </div>
-            <div>
-              <label
-                className="mb-1 block text-sm font-medium text-gray-700"
-                htmlFor="password"
-              >
-                Password
-              </label>
-              <input
-                id="password"
-                className="form-input w-full py-2"
-                type="password"
-                autoComplete="on"
-                placeholder="••••••••"
-                required
-              />
-            </div>
-          </div>
-          <div className="mt-6">
-            <button className="btn w-full bg-gradient-to-t from-blue-600 to-blue-500 bg-[length:100%_100%] bg-[bottom] text-white shadow hover:bg-[length:100%_150%]">
-              Sign In
-            </button>
-          </div>
-        </form>
-        {/* Bottom link */}
-        <div className="mt-6 text-center">
-          <Link
-            className="text-sm text-gray-700 underline hover:no-underline"
-            href="/reset-password"
-          >
-            Forgot password
-          </Link>
-        </div>
-      </>
-    </>
-  );
-}
+<body>
+  <h1>欢迎来到我的网页</h1>
+  <p>这是一些简单的内容。</p>
+</body>
+
+</html>
